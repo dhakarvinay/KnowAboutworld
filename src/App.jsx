e07398a484990e67565f6e3d0858b0b2d42e5d5b
dhakarvinay <146158255+dhakarvinay@users.noreply.github.com>
2025-12-12 08:58:40 +0530

    Fix import path for App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { AppLayout } from "./component/layout/AppLayout";
 
-import "./app.css";
+import "./App.css";
 
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
